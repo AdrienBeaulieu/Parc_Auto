98eda9bfeab38d864319f64335d06b2ec9b5c37e
Adrien BEAULIEU <a.beaulieu@GRN164.local>
2020-11-19 13:17:49 +0100

    Création class + nom variables

--- Voiture.class.js
+++ Voiture.class.js
@@ -1,1 +1,14 @@
-alert("ok");
+function Voiture(uneImmatriculation, uneCouleur, unPoids, unePuissance, uneCapRéservoir,unNbrPlaces) {
+
+    var immatriculation = uneImmatriculation;
+    var couleur = uneCouleur;
+    var poids = unPoids;
+    var puissance = unePuissance;
+    var capacitéRéservoir = uneCapRéservoir;
+    var nbrPlaces = unNbrPlaces;
+}
+
+
+
+
+// alert("ok");
