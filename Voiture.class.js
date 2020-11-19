62e69aa5bc207a2e873d6c200ebc793072fda8c3
Adrien BEAULIEU <a.beaulieu@GRN164.local>
2020-11-19 13:40:32 +0100

    Ajout vérif sur Immat + couleur + (Get et Set)

--- Voiture.class.js
+++ Voiture.class.js
@@ -9,6 +9,30 @@ function Voiture(uneImmatriculation, uneCouleur, unPoids, unePuissance, uneCapR�
     var nbrPlaces = unNbrPlaces; //float
     var assuré = false; // bool has a default setting ( false )
     var msg = "Bienvenue à bord du véhicule"; // string has a defautl setting
+
+    this.setImmat = function(uneImmatriculation) {
+        if(!uneImmatriculation) throw new Error("Veuillez entrer une Immatriculation");
+        if(!(isNaN(uneImmatriculation))) throw new Error("Veuillez entrer une Immatriculation correcte");
+        if(uneImmatriculation.length < 2) throw new Error("Veuillez entrer une Immatriculation d'au moins 2 caractères");
+        immatriculation = uneImmatriculation;
+    }
+
+    this.setCouleur = function(uneCouleur) {
+        if(!uneCouleur) throw new Error("Veuillez entrer une couleur");
+        if(!(isNaN(uneCouleur))) throw new Error("Veuillez entrer une couleur correcte");
+        if(uneCouleur.length < 2) throw new Error("Veuillez entrer une couleur d'au moins 2 caractères");
+        couleur = uneCouleur;
+    }
+
+    this.setImmat(uneImmatriculation);
+    this.setCouleur(uneCouleur);
+
+    this.getImmat = function() {
+        return immatriculation;
+    }
+    this.getCouleur = function() {
+        return couleur;
+    }
 }
 
 
--- parc_auto.js
+++ parc_auto.js
@@ -1,1 +1,15 @@
-alert("ok");
+// création voiture 1
+try {
+    var voiture1 = new Voiture("AA - 000 - AA", "bleu", 750, 90, 50.3, 5)
+} catch (e) {
+    console.error(e.message);
+}
+
+console.log(voiture1);
+
+console.log(voiture1.getCouleur());
+console.log(voiture1.getImmat());
+
+
+
+// alert("ok");
