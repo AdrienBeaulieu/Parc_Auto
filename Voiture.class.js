ae86e7a7015b9b9f93145bfb4db372ab95b646e7
Adrien BEAULIEU <a.beaulieu@GRN164.local>
2020-11-19 13:24:13 +0100

    Ajout variables

--- Voiture.class.js
+++ Voiture.class.js
@@ -1,11 +1,14 @@
 function Voiture(uneImmatriculation, uneCouleur, unPoids, unePuissance, uneCapRéservoir,unNbrPlaces) {
 
-    var immatriculation = uneImmatriculation;
-    var couleur = uneCouleur;
-    var poids = unPoids;
-    var puissance = unePuissance;
-    var capacitéRéservoir = uneCapRéservoir;
-    var nbrPlaces = unNbrPlaces;
+
+    var immatriculation = uneImmatriculation; // string
+    var couleur = uneCouleur; // string
+    var poids = unPoids; // int
+    var puissance = unePuissance; // int
+    var capacitéRéservoir = uneCapRéservoir; //float
+    var nbrPlaces = unNbrPlaces; //float
+    var assuré = false; // bool has a default setting ( false )
+    var msg = "Bienvenue à bord du véhicule"; // string has a defautl setting
 }
 
 
